c3866358d4d050537cdcf9132249e6fb003f3803
tiagolima2005 <tilima.steam@gmail.com>
2024-11-20 19:09:43 +0000

    add students function done

--- AcademicGradeManager.c
+++ AcademicGradeManager.c
@@ -5,7 +5,9 @@ struct Student
 {
     char name[100];
     char birth_date[11]; // dd/mm/yyyy + '\0'
-    int age;
+    int year;
+    char class;
+    int number;
 };
 
 void add_students(struct Student *student);
@@ -13,6 +15,8 @@ void add_students(struct Student *student);
 int main()
 {
     int option;
+    struct Student student;
+
     do
     {
         printf("Welcome to the Academic Grade Manager Program!\n");
@@ -22,10 +26,13 @@ int main()
         printf("3 - Calculate the average grade\n");
         printf("4 - See passed and failed students\n");
         printf("5 - Exit program\n");
+        scanf("%d", &option);
+        getchar();
+
         switch (option)
         {
         case 1:
-            /* code */
+            add_students(&student);
             break;
 
         default:
@@ -41,4 +48,15 @@ void add_students(struct Student *student)
     printf("Insert the student's name:\n");
     fgets(student->name, sizeof(student->name), stdin);
     student->name[strcspn(student->name, "\n")] = '\0';
+
+    printf("Insert the student's year:\n");
+    scanf("%d", &student->year);
+    getchar();
+
+    printf("Insert the student's class:\n");
+    scanf(" %c", &student->class);
+    getchar();
+
+    printf("Insert the student's number/id:\n");
+    scanf("%d", &student->number);
 }
